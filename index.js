05a887b60fff746112732fe587abe1c6023c3ef8
Jovan <mariothrowsfieball@gmail.com>
2022-09-05 08:37:49 +0200

    created a server so it won't ask me for camera permissions on every reload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.get("/", (req, res) => {
+	res.sendFile("index.html", { root: path.join(__dirname) });
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
